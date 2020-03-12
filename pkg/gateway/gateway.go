02e44db569a0f2672b645020fad00eee1ff2e302
Rafael <rafa@blackmagicbox.com.br>
2020-03-12 23:15:58 +0100

    Feat basic http server config.

---
+++ pkg/gateway/gateway.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func App(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Consumer API Gateway"))
+}
+
+func main()  {
+	println("Gateway running on: localhost:8080")
+	http.HandleFunc("/", App)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
