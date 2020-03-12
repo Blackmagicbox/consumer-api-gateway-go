1b11e29e077538dcc5346646318e4781e9cd6387
Rafael <rafa@blackmagicbox.com.br>
2020-03-13 00:14:59 +0100

    Feat router manager stdlib [DRAFT]

--- pkg/gateway/gateway.go
+++ pkg/gateway/gateway.go
@@ -8,6 +8,7 @@ func App(w http.ResponseWriter, r *http.Request) {
 
 func main()  {
 	println("Gateway running on: localhost:8080")
+
 	http.HandleFunc("/", App)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
---
+++ pkg/router/router.go
@@ -0,0 +1,5 @@
+package router
+
+func main() {
+	// Implement the selectors for routes
+}
